5ae9c697910c7a2dc4d0d737b4b5ebfdbeb195dd
Daniel Bidulock <dan.bidulock@hiregroundsoftware.com>
2013-11-19 10:32:07 -0700

    Get partial init function with tests

--- app/scripts/controllers/account.js
+++ app/scripts/controllers/account.js
@@ -1,10 +1,23 @@
 'use strict';
 
 angular.module('geboRegistrantHaiApp')
-  .controller('AccountCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('AccountCtrl', function ($scope, Token) {
+
+    $scope.friends = [];
+
+    /**
+     * init
+     */
+    $scope.init = function() {
+        Token.request({
+                action: 'ls',
+                resource: 'friends',
+                fields: ['name', '_id', 'email'],
+          }).
+        then(function(friends) {
+            $scope.friends = friends;
+          }).
+        catch(function(err) {
+          }); 
+      };
   });
--- test/spec/controllers/account.js
+++ test/spec/controllers/account.js
@@ -2,21 +2,98 @@
 
 describe('Controller: AccountCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('geboRegistrantHaiApp'));
+    /**
+     * Constants
+     */
+    var GEBO_ADDRESS = 'https://somegebo.com',
+        REDIRECT_URI = 'https://myhost.com',
+        ACCESS_TOKEN = '1234';
 
-  var AccountCtrl,
-    scope;
+    // load the controller's module
+    beforeEach(module('geboRegistrantHaiApp'));
+    
+    var AccountCtrl,
+        token,
+        scope,
+        $httpBackend;
+    
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller, $rootScope, $injector) {
+        scope = $rootScope.$new();
+        token = $injector.get('Token');
+        $httpBackend = $injector.get('$httpBackend');
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    AccountCtrl = $controller('AccountCtrl', {
-      $scope: scope
+        // Set up token
+        token.setEndpoints({
+                clientId: 'gebo-registrant-hai@capitolhill.ca',
+                clientName: 'gebo-registrant-hai',
+                localStorageName: 'gebo-registrant-hai-token',
+                gebo: GEBO_ADDRESS,
+                redirect: REDIRECT_URI,
+              });
+        token.set(ACCESS_TOKEN);
+        
+        // Get controller
+        AccountCtrl = $controller('AccountCtrl', {
+                $scope: scope,
+                Token: token,
+              });
+
+        /**
+         * $httpBackend
+         */
+
+        // Get a list of friends
+        $httpBackend.whenPOST(GEBO_ADDRESS + '/request', {
+                action: 'ls',
+                resource: 'friends',
+//                recipient: 'my@address.com', 
+                fields: ['name', '_id', 'email'],
+                access_token: ACCESS_TOKEN,
+            }).respond([{ name: 'Dan', _id: '1', email: 'dan@email.com'},
+                        { name:'Yanfen', _id: '2', email: 'yanfen@email.com' }]);
+
+    }));
+
+    afterEach(function() {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
     });
-  }));
+    
+    it('should do something', function () {
+      expect(!!AccountCtrl).toBe(true);
+      expect(!!token).toBe(true);
+    });
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
+    /**
+     * init
+     */
+    describe('init', function() {
+
+//        beforeEach(function() {
+//        });
+
+        it('should load a list of friends', function() {
+            expect(scope.friends.length).toBe(0);
+            $httpBackend.expectPOST(GEBO_ADDRESS + '/request', {
+                    action: 'ls',
+                    resource: 'friends',
+//                    recipient: 'my@address.com', 
+                    fields: ['name', '_id', 'email'],
+                    access_token: ACCESS_TOKEN });
+
+            scope.init();
+            $httpBackend.flush();
+
+            expect(scope.friends.length).toBe(2);
+            expect(scope.friends[0].name).toBe('Dan');
+            expect(scope.friends[0].email).toBe('dan@email.com');
+            expect(scope.friends[1].name).toBe('Yanfen');
+            expect(scope.friends[1].email).toBe('yanfen@email.com');
+        });
+
+        it('should load a list of social commitments', function() {
+
+        });
+    });
 });
